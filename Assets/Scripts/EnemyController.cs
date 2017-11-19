e575b5b834ad7befb0fad5670d24996d50cd0dac
Bradley Shively <bshively@gmail.com>
2017-11-19 12:48:07 -0500

    Implement simple enemy follow. Enemies will walk in the x direction of the player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private Rigidbody2D body;
+    private GameObject player;
 
     // Use this for initialization
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
     }
 	
     // Update is called once per frame
     void Update()
     {
-        body.velocity = new Vector2(-1, body.velocity.y);
+        float direction = Mathf.Sign(player.transform.position.x - gameObject.transform.position.x);
+        body.velocity = new Vector2(1 * direction, body.velocity.y);
     }
 
 
