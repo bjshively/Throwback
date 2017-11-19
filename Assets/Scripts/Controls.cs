34c626f9536c4ae20f4823de2408c1d40cc3e99c
Bradley Shively <bshively@gmail.com>
2017-11-18 22:11:45 -0500

    Implement basic firing functionality

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     public LayerMask ground;
-
+    private Vector3 startLocation;
     protected Rigidbody2D body;
 
+
+
+    // Player attributes
+    private int health = 3;
     private float horizontalSpeed = 12.0F;
     private bool grounded;
 
-    private Vector3 startLocation;
+
 
     // Use this for initialization
     void Start()
     {
         startLocation = transform.position;
-
         body = GetComponent<Rigidbody2D>();
         grounded = IsGrounded();      
     }
@@ -43,7 +46,13 @@ public class Controls : MonoBehaviour
         {
             body.AddForce(new Vector2(0, 500), ForceMode2D.Impulse);
         }
-            
+
+        // Shoot
+        if (Input.GetKeyDown("x"))
+        {
+            Instantiate(Resources.Load("bullet"));
+        }
+
         // For testing/during dev
         // Respawn player at start location if they fall below y=-10
         if (transform.position.y < -10)
---
+++ Assets/Scripts/bulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = GameObject.Find("Player").transform.position;
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(30, 0);    
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
