40a05706daeed58daaaf278beb14d5645d81b85b
Bradley Shively <bshively@gmail.com>
2017-11-18 21:00:36 -0500

    Implement simple camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private float xMax;
+
+    [SerializeField]
+    private float xMin;
+
+    [SerializeField]
+    private float yMax;
+
+    [SerializeField]
+    private float yMin;
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Sets a reference to the player
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Follows the player
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), -10);
+    }
+}
