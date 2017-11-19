e2bae7f55fc9c388e8fe3426bd69109f5abe4f7f
Bradley Shively <bshively@gmail.com>
2017-11-18 22:18:04 -0500

    Update comments in bullet script

--- Assets/Scripts/bulletController.cs
+++ Assets/Scripts/bulletController.cs
@@ -10,7 +10,13 @@ public class bulletController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        // Bullets are destroyed after 1 second automatically
+        Destroy(gameObject, 1);
+
+        // Fire the bullet out of the player's position
+        // May want to update to some gun barrel position eventually
         transform.position = GameObject.Find("Player").transform.position;
+
         body = GetComponent<Rigidbody2D>();
         body.velocity = new Vector2(30, 0);    
     }
@@ -18,6 +24,6 @@ public class bulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		
+	
     }
 }
