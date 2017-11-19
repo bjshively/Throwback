9ebfc11f9ab7e119eec38b520286076a06724327
Bradley Shively <bshively@gmail.com>
2017-11-19 03:14:28 -0500

    Implement basic enemy forward movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = new Vector2(-1, body.velocity.y);
+    }
+
+
+    void OnBecameVisible()
+    {
+
+    }
+
+}
