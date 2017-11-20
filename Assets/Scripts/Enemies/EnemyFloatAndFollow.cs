ce9c80dd22b4aa2eea76a0b94c6f8c5c7ab0a8ca
Bradley Shively <bshively@gmail.com>
2017-11-19 22:22:37 -0500

    Modify Enemy Float and Follow move code to smooth follow pathway by checking target location less frequently

--- Assets/Scripts/Enemies/EnemyFloatAndFollow.cs
+++ Assets/Scripts/Enemies/EnemyFloatAndFollow.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class EnemyFloatAndFollow : Enemy
 {
+
+    private Vector2 targetLocation;
+    private float timer;
+
     public virtual float moveSpeed
     {
         get { return .5f; }
     }
 
+    void Start()
+    {
+        base.Start();
+        UpdateTarget();
+    }
+
+    void Update()
+    {
+        Move();
+
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            UpdateTarget();
+            timer = 0;
+        }
+
+    }
+
     // Float towards the player
     protected override void Move()
     {
-        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), player.transform.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), targetLocation, moveSpeed * Time.deltaTime);
+    }
+
+    protected void UpdateTarget()
+    {
+        targetLocation = player.transform.position;
     }
 
 }
