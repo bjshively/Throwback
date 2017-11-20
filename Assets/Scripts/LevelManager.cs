c8798bcd33f9be7de1ecaa84646737fd8530f54b
Bradley Shively <bshively@gmail.com>
2017-11-20 01:57:49 -0500

    Rename Main scene to 1. Create shell level manager script for future use

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public string levelToLoad;
+
+    // Use this for initialization
+    void Start()
+    {
+		
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+    void OnTrigger2DEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
