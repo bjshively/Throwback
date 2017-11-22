0d7f200e944a3f208a8943a1122a06303a5e3acd
Eric Mofield <ericmofield@gmail.com>
2017-11-22 18:21:42 -0500

    Clean up assets folder. Add _TODO.cs in /scripts.

---
+++ Assets/Scripts/_TODO.cs
@@ -0,0 +1,12 @@
+﻿// EMO TODO
+// melee sprites
+// enemy sprites that shoot projectiles
+// crumbling sprite that reappears
+// visually distinct platform sprites
+// Animate tetris sprites as pickups
+// end of level rocket
+// explosion animation
+// start thinking about an opening animation
+
+// reappearing blocks
+// enemies 
