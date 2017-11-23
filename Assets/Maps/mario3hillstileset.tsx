d9acafc284c76638a5febd603040fd1cb93965b0
Eric Mofield <ericmofield@gmail.com>
2017-11-23 02:04:03 -0500

    Clean up Project hierarchy. Import Tiled2Unity settings. Update prologue map and Import. Create camera prefab.

--- Assets/Maps/mario3hillstileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Mario 3 Levels" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1533" columns="73">
- <image source="../Sprites/level/mario3levels.png" width="1242" height="358"/>
-</tileset>
---
+++ Assets/TiledMaps/tilesets/Mario 3 Levels.tsx
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mario 3 Levels" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1533" columns="73">
+ <image source="../../Sprites/level/mario3levels.png" width="1242" height="358"/>
+ <tile id="154">
+  <objectgroup draworder="index">
+   <object id="2" x="16" y="0">
+    <polygon points="0,0 0,16 -6,16 -16,6 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="155">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="156">
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="302">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="645">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="860">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="861">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="862">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="921">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="975">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="976">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="977">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1118">
+  <objectgroup draworder="index">
+   <object id="7" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1119">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1120">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1191">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1192">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1193">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1261">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="1262">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="1263">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="1264">
+  <objectgroup draworder="index">
+   <object id="1" x="16" y="8">
+    <polygon points="0,0 0,8 -16,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1265">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8">
+    <polygon points="0,0 0,8 16,8 16,-8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1266">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="16">
+    <polygon points="0,0 16,-16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1337">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,8 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1338">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8">
+    <polygon points="0,0 16,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1339">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1417">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1418">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1490">
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="1491">
+  <objectgroup draworder="index"/>
+ </tile>
+</tileset>
