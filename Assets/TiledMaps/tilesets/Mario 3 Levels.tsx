a431e0900ef0e5c1c5036e2ae262d73003e49afb
Eric Mofield <ericmofield@gmail.com>
2017-11-23 15:16:28 -0500

    Update tileset to include collision layer and test import. Add melee and item animations.

--- Assets/TiledMaps/tilesets/Mario 3 Levels.tsx
+++ Assets/TiledMaps/tilesets/Mario 3 Levels.tsx
@@ -3,6 +3,9 @@
  <image source="../../Sprites/Level/mario3levels.png" width="1242" height="358"/>
  <tile id="154">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="platforms"/>
+   </properties>
    <object id="2" x="16" y="0">
     <polygon points="0,0 0,16 -6,16 -16,6 -16,0"/>
    </object>
@@ -10,6 +13,9 @@
  </tile>
  <tile id="155">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="platforms"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
@@ -18,6 +24,9 @@
  </tile>
  <tile id="302">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="platforms"/>
+   </properties>
    <object id="1" x="0" y="0">
     <polygon points="0,0 0,16 16,0"/>
    </object>
@@ -25,61 +34,97 @@
  </tile>
  <tile id="645">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="platforms"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="860">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="861">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="862">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="9"/>
   </objectgroup>
  </tile>
  <tile id="921">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="Hazards"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="975">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="MovingPlatforms"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="976">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="MovingPlatforms"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="977">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="MovingPlatforms"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1118">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="7" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1119">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1120">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="2" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1191">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
@@ -90,26 +135,41 @@
  </tile>
  <tile id="1193">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="3" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1261">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="2" x="0" y="0" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="1262">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="1263">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="2" x="0" y="0" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="1264">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="16" y="8">
     <polygon points="0,0 0,8 -16,8"/>
    </object>
@@ -117,6 +177,9 @@
  </tile>
  <tile id="1265">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="8">
     <polygon points="0,0 0,8 16,8 16,-8"/>
    </object>
@@ -124,6 +187,9 @@
  </tile>
  <tile id="1266">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="16">
     <polygon points="0,0 16,-16 16,0"/>
    </object>
@@ -131,6 +197,9 @@
  </tile>
  <tile id="1337">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0">
     <polygon points="0,0 16,8 16,16 0,16"/>
    </object>
@@ -138,6 +207,9 @@
  </tile>
  <tile id="1338">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="8">
     <polygon points="0,0 16,8 0,8"/>
    </object>
@@ -145,6 +217,9 @@
  </tile>
  <tile id="1339">
   <objectgroup draworder="index">
+   <properties>
+    <property name="unity:layer" value="ground"/>
+   </properties>
    <object id="1" x="0" y="0">
     <polygon points="0,0 16,16 0,16"/>
    </object>
