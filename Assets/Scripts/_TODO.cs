4a0257fb60ae08cefb76075b8c244e5ebf2b3f81
Eric Mofield <ericmofield@gmail.com>
2017-11-23 18:16:17 -0500

    Start working on player melee sprites. Add some TODOs.

--- Assets/Scripts/_TODO.cs
+++ Assets/Scripts/_TODO.cs
@@ -1,12 +1,19 @@
 ﻿// EMO TODO
-// melee sprites
+// finish melee sprite
 // enemy sprites that shoot projectiles
 // crumbling sprite that reappears
+// explosion animation
+// start thinking about an opening animation
+// reappearing blocks
+// enemies 
+// powerglove sprite
+// update player sprite for stand and run without glove
+
+
+
+
+// DONE =================
+// melee sprites
 // visually distinct platform sprites
 // Animate tetris sprites as pickups
 // end of level rocket
-// explosion animation
-// start thinking about an opening animation
-
-// reappearing blocks
-// enemies 
