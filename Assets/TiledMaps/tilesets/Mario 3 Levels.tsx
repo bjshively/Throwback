e499ccb9e4a45bf2e2fa97e5c385e2c92a34d7bb
Eric Mofield <ericmofield@gmail.com>
2017-11-23 02:57:44 -0500

    Continue to test and polish the import process. Still cannot get player to react to new environment.
    Tiled map param
    unity:scale = .0116

--- Assets/TiledMaps/tilesets/Mario 3 Levels.tsx
+++ Assets/TiledMaps/tilesets/Mario 3 Levels.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Mario 3 Levels" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1533" columns="73">
- <image source="../../Sprites/level/mario3levels.png" width="1242" height="358"/>
+ <image source="../../Sprites/Level/mario3levels.png" width="1242" height="358"/>
  <tile id="154">
   <objectgroup draworder="index">
    <object id="2" x="16" y="0">
