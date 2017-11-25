606b551073907cd281160e30abefd13e087e5691
Bradley Shively <bshively@gmail.com>
2017-11-25 16:54:42 -0500

    Implement basic Contra prefab

---
+++ Assets/Contra.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contra : Enemy
+{
+    private float timer = 0;
+    private Vector2 target;
+    private Vector3 barrelPoint;
+	
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+    //
+    //    }
+
+    protected override void Move()
+    {
+        playerDirection = Mathf.Sign(player.transform.position.x - gameObject.transform.position.x);
+        Flip();
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            Shoot();
+            timer = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        barrelPoint = transform.Find("barrel").transform.position;
+        GameObject bullet = Instantiate(Resources.Load("fireball") as GameObject);
+        bullet.transform.position = barrelPoint;
+    }
+}
---
+++ Assets/Fireball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    private Vector3 target;
+    private float moveSpeed;
+    private PlayerController pc;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+        target = GameObject.Find("Player").transform.position;
+        moveSpeed = 2;
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), target, moveSpeed * Time.deltaTime);
+        if (transform.position == target)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Destroy when bullet hits the ground
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Damage the player on contact
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            pc.takeDamage();
+        }
+    }
+}
