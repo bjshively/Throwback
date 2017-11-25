fff93b6faceb9bd25ef6c1de2a6ffe4ffda4a0f5
Bradley Shively <bshively@gmail.com>
2017-11-25 01:57:21 -0500

    Rewrite the rocket and cage code I somehow forgot to commit and then git cleaned

---
+++ Assets/Scripts/Cage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cage : MonoBehaviour
+{
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+    public void Unlock()
+    {
+        anim.SetTrigger("fall");
+        Invoke("Disable", 1);
+    }
+
+    private void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D body;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Launch();
+        }
+    }
+
+    void Launch()
+    {
+        player.SetActive(false);
+        body.gravityScale = 0;
+        anim.SetTrigger("FinishLevel");
+        body.velocity = Vector2.up;
+    }
+}
