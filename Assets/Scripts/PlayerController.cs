a8f9638a53e57d40fb5f62fe935d8e1d9928c28c
Eric Mofield <ericmofield@gmail.com>
2017-11-26 00:29:44 -0500

    Update some more sort ordering. Update Contra animator with death (still not working).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -115,7 +115,7 @@ public class PlayerController : MonoBehaviour
                 if (Input.GetKey("x") && canFire)
                 {
                     // Spawn an instance of the bullet prefab
-                    Instantiate(Resources.Load("pistol"));
+                    Instantiate(Resources.Load("Lemon"));
                     canFire = false;
                     anim.SetTrigger("zap");
                 }
@@ -144,7 +144,7 @@ public class PlayerController : MonoBehaviour
                 {
                     scopeIsCool = false;
                     Invoke("resetScopeCool", scopeCooldownTime);
-                    Instantiate(Resources.Load("superScopeShot"));
+                    Instantiate(Resources.Load("SuperScope"));
                     canFire = false;
                     anim.SetTrigger("scope");
                 }
