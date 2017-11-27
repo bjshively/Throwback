69e5586fb9c651ba1b530f3687d2c6f871b3feb6
Eric Mofield <ericmofield@gmail.com>
2017-11-27 00:21:29 -0500

    Attempts at fixing map export auto settings.

--- Assets/TiledMaps/tilesets/Mario 3 Levels.tsx
+++ Assets/TiledMaps/tilesets/Mario 3 Levels.tsx
@@ -40,39 +40,22 @@
  </tile>
  <tile id="860">
   <objectgroup draworder="index">
-   <properties>
-    <property name="unity:layer" value="ground"/>
-   </properties>
-   <object id="1" x="0" y="0" width="16" height="8"/>
+   <object id="1" type="jumpthrough" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="861">
   <objectgroup draworder="index">
-   <properties>
-    <property name="unity:layer" value="ground"/>
-   </properties>
-   <object id="1" x="0" y="0" width="16" height="8"/>
+   <object id="1" type="jumpthrough" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="862">
   <objectgroup draworder="index">
-   <properties>
-    <property name="unity:layer" value="ground"/>
-   </properties>
-   <object id="1" x="0" y="0" width="16" height="9"/>
+   <object id="1" type="jumpthrough" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="921">
   <objectgroup draworder="index">
-   <properties>
-    <property name="unity:layer" value="ground"/>
-    <property name="unity:tag" value="hazard"/>
-   </properties>
-   <object id="1" x="0" y="0" width="16" height="16">
-    <properties>
-     <property name="unity:tag" value="Wall"/>
-    </properties>
-   </object>
+   <object id="1" type="Hazard" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="975">
@@ -101,26 +84,26 @@
  </tile>
  <tile id="1118">
   <objectgroup draworder="index">
-   <object id="7" name="Ground" type="ground" x="1" y="0" width="15" height="2"/>
-   <object id="8" name="Wall" type="Wall" x="-1" y="3" width="2" height="13"/>
+   <object id="7" type="ground" x="1" y="0" width="15" height="2"/>
+   <object id="8" type="Wall" x="-1" y="4" width="2" height="12"/>
    <object id="9" type="ground" x="0" y="3" width="2" height="13"/>
   </objectgroup>
  </tile>
  <tile id="1119">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="0" y="0" width="16" height="2"/>
+   <object id="1" type="ground" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="1120">
   <objectgroup draworder="index">
-   <object id="2" name="Ground" type="ground" x="0" y="0" width="15" height="2"/>
-   <object id="3" name="Wall" type="Wall" x="15" y="3" width="2" height="13"/>
+   <object id="2" type="ground" x="0" y="0" width="15" height="2"/>
+   <object id="3" type="Wall" x="15" y="3" width="2" height="13"/>
    <object id="4" type="ground" x="14" y="3" width="2" height="13"/>
   </objectgroup>
  </tile>
  <tile id="1191">
   <objectgroup draworder="index">
-   <object id="1" name="Wall" type="Wall" x="-1" y="0" width="2" height="16"/>
+   <object id="1" type="Wall" x="-1" y="2" width="2" height="14"/>
    <object id="2" type="ground" x="0" y="0" width="2" height="16"/>
   </objectgroup>
  </tile>
@@ -129,86 +112,77 @@
  </tile>
  <tile id="1193">
   <objectgroup draworder="index">
-   <object id="4" name="Wall" type="Wall" x="15" y="0" width="2" height="16"/>
+   <object id="4" type="Wall" x="15" y="2" width="2" height="14"/>
    <object id="5" type="ground" x="14" y="0" width="2" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1261">
   <objectgroup draworder="index">
-   <properties>
-    <property name="unity:layer" value="ground"/>
-   </properties>
-   <object id="2" x="0" y="0" width="16" height="8"/>
+   <object id="2" type="jumpthrough" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="1262">
   <objectgroup draworder="index">
-   <properties>
-    <property name="unity:layer" value="ground"/>
-   </properties>
-   <object id="1" x="0" y="0" width="16" height="8"/>
+   <object id="1" type="jumpthrough" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="1263">
   <objectgroup draworder="index">
-   <properties>
-    <property name="unity:layer" value="ground"/>
-   </properties>
-   <object id="2" x="0" y="0" width="16" height="8"/>
+   <object id="2" type="jumpthrough" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="1264">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="16" y="8">
+   <object id="1" type="ground" x="16" y="8">
     <polygon points="0,0 0,8 -16,8"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1265">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="0" y="8">
+   <object id="1" type="ground" x="0" y="8">
     <polygon points="0,0 0,8 16,8 16,-8"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1266">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="0" y="16">
+   <object id="1" type="ground" x="0" y="16">
     <polygon points="0,0 16,-16 16,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1337">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="0" y="0">
+   <object id="1" type="ground" x="0" y="0">
     <polygon points="0,0 16,8 16,16 0,16"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1338">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="0" y="8">
+   <object id="1" type="ground" x="0" y="8">
     <polygon points="0,0 16,8 0,8"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1339">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="0" y="0">
+   <object id="1" type="ground" x="0" y="0">
     <polygon points="0,0 16,16 0,16"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1417">
   <objectgroup draworder="index">
-   <object id="2" name="Ground" type="ground" x="1" y="0" width="15" height="2"/>
+   <object id="2" type="ground" x="1" y="0" width="15" height="2"/>
    <object id="3" type="Wall" x="-1" y="3" width="2" height="13"/>
    <object id="4" type="ground" x="0" y="3" width="2" height="13"/>
   </objectgroup>
  </tile>
  <tile id="1418">
   <objectgroup draworder="index">
-   <object id="1" name="Ground" type="ground" x="0" y="0" width="16" height="2"/>
+   <object id="1" type="ground" x="0" y="0" width="16" height="2"/>
   </objectgroup>
  </tile>
  <tile id="1490">
