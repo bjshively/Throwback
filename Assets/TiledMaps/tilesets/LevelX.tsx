7046b2578d9ca7822e218a599b0062253b604592
Eric Mofield <ericmofield@gmail.com>
2017-11-27 23:25:43 -0500

    Build a tileset for another level. Start mapping out in Tiled.

---
+++ Assets/TiledMaps/tilesets/LevelX.tsx
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="LevelX" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1088" columns="32">
+ <image source="../../Sprites/Level/LevelX.png" width="545" height="589"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="16">
+    <polygon points="0,0 16,-16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0">
+    <polygon points="0,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="16">
+    <polygon points="0,0 16,-8 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="8">
+    <polygon points="0,0 16,-8 16,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="102">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0">
+    <polygon points="0,0 16,8 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="8">
+    <polygon points="0,0 16,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup draworder="index">
+   <object id="1" type="jumpthrough" x="0" y="0" width="16" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup draworder="index">
+   <object id="1" type="jumpthrough" x="0" y="0" width="16" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index">
+   <object id="1" type="jumpthrough" x="0" y="0" width="16" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="128">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0">
+    <polygon points="0,0 16,16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="129">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="16">
+    <polygon points="0,0 16,-16 0,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="130">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0">
+    <polygon points="0,0 16,8 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="131">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="8">
+    <polygon points="0,0 16,8 16,-8 0,-8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="134">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="16">
+    <polygon points="0,0 16,-8 16,-16 0,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="135">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="8">
+    <polygon points="0,0 16,-8 0,-8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="160">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="161">
+  <objectgroup draworder="index">
+   <object id="2" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="162">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="192">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="194">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="224">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="225">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="226">
+  <objectgroup draworder="index">
+   <object id="1" type="ground" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="288">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="11"/>
+  </objectgroup>
+ </tile>
+</tileset>
