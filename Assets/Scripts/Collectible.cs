c7593712f13ab53c9c07b44a18f0a2bae654f316
Bradley Shively <bshively@gmail.com>
2017-11-28 01:52:22 -0500

    Remove old TODO

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -39,8 +39,7 @@ public class Collectible : MonoBehaviour
             {
                 audio.Play();
             }
-
-            // TODO: Create a "key" tag that can be applied to any collectible to make it unlock the rocket
+                
             if (gameObject.tag == "key")
             {
                 GameObject.Find("Cage").GetComponent<Cage>().Invoke("Unlock", 2.18f);
