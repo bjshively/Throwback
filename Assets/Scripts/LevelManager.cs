8fa7c2fd36bb5bf20f3b546103bf1d26baf0ff97
Bradley Shively <bshively@gmail.com>
2017-11-30 17:38:20 -0500

    Add exit doors to first level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -85,7 +85,6 @@ public class LevelManager : MonoBehaviour
 
     public void AddPiece()
     {
-        Debug.Log("blah");
         collectedPieces++;
         exitDoor.SetInteger("pieces", collectedPieces);
     }
