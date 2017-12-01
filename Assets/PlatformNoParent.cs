be60de6b21e92246464f8fac56fcce919768fe94
Bradley Shively <bshively@gmail.com>
2017-12-01 01:14:09 -0500

    Create PlatformNoParent for timed wall platforms

---
+++ Assets/PlatformNoParent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformNoParent : MonoBehaviour
+{
+    protected float speed = 1;
+    protected Transform currentPoint;
+    public Transform[] points;
+    private float timer;
+    public float pause;
+
+    // Begin by traveling towards the end point
+    public int pointSelection = 1;
+
+    protected void Start()
+    {
+        timer = 0;
+        currentPoint = points[pointSelection];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!LevelManager.Instance.stopAllAction())
+        {
+            
+            Move();
+        }
+    }
+
+    protected virtual void Move()
+    {
+        
+        transform.position = Vector2.MoveTowards(transform.position, currentPoint.transform.position, speed * Time.deltaTime);
+        if (transform.position == currentPoint.transform.position)
+        {
+            timer += Time.deltaTime;
+            if (timer >= pause)
+            {
+                Flip();
+                timer = 0;
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        pointSelection++;
+        if (pointSelection == points.Length)
+        {
+            pointSelection = 0;
+
+        }
+        currentPoint = points[pointSelection];
+    }
+}
