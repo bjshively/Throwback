75c3b847938b216946681ad9b75c6b36e79c89ff
Bradley Shively <bshively@gmail.com>
2017-11-30 20:07:12 -0500

    Setup door eating the world animation on player collision

---
+++ Assets/ExitDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+    private Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            if (anim.GetBool("complete"))
+            {
+                anim.SetTrigger("eat");
+            }
+        }
+    }
+  
+}
