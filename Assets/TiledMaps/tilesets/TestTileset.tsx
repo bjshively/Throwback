f84dd1fb504ee46297f9d2ff0ca407f8660b17b6
Eric Mofield <ericmofield@gmail.com>
2017-12-11 20:59:30 -0500

    Organizing and testing some new map stuff.

---
+++ Assets/TiledMaps/tilesets/TestTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Grass" tilewidth="16" tileheight="16" spacing="1" tilecount="81" columns="9">
+ <image source="../../Sprites/Level/Tileset.png" width="160" height="160"/>
+</tileset>
